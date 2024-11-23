5171c396976c417fb5b7d2603ff142cf67087486
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-11-23 10:50:02 -0300

    empezando a añadir el peso de cada conexion

--- incs/header.h
+++ incs/header.h
@@ -15,12 +15,21 @@ typedef struct Graph
 {
     int users_number;
     User users[MAX_USERS];
-    int *adyacent_friendship_list[MAX_USERS]; // lista de adyacencia
+    Edge *adyacent_friendship_list[MAX_USERS]; // lista de adyacencia
+    int friends_count[MAX_USERS];              // contador de amigos de un usuario
 } Graph;
+
+typedef struct Edge
+{
+    int dest;   // usuario al que esta conectado
+    int weight; // peso de la conexion
+
+} Edge;
 
 // funciones de los grafos
 void initialize_graph(Graph *graph);
 int add_user(Graph *graph, const char *name);
-void add_friendship(Graph *graph, int user1, int user2);
+void add_friendship(Graph *graph, int user1, int user2, int weigth);
 void remove_user(Graph *graph, int user_id);
 void display_friends(Graph *graph, int user_id);
+void remove_friendship(Graph *graph, int user1, int user2);
--- src/graph.c
+++ src/graph.c
@@ -29,7 +29,7 @@ int add_user(Graph *graph, const char *name)
 //                |   |       |
 //                v   v       v
 //              [2|3] NULL   [3|4]
-void add_friendship(Graph *graph, int user1, int user2)
+void add_friendship(Graph *graph, int user1, int user2, int weight)
 {
     if (user1 == user2)
     {
@@ -37,18 +37,23 @@ void add_friendship(Graph *graph, int user1, int user2)
         return;
     }
 
-    // puntero al arreglo de amigos del user1
-    int *friends_of_user1 = graph->adyacent_friendship_list[user1];
-    // se añade con realloc dinamicamente un nuevo espacio en el arreglo de amigos
-    friends_of_user1 = realloc(friends_of_user1, sizeof(int) * (graph->users_number + 1));
-    // se inserta el nuevo amigo en la lista del user1, graph->users_number es el indice en el cual se insertara el nuevo amigo
-    friends_of_user1[graph->users_number] = user2;
-    // actualiza la lista de adyacencia para que apunte a los amigos de user1
+    // se obtiene la lista de amigos de user1
+    Edge *friends_of_user1 = graph->adyacent_friendship_list[user1];
+    // se guardan los amigos totales del user1
+    int current_size1 = graph->friends_count[user1];
+    // redimensiona el arrelgo dinamico friends_of_user1 para añadir un nuevo amigo (conexion)
+    friends_of_user1 = realloc(friends_of_user1, sizeof(Edge) * (current_size1 + 1));
+    // añade como destino en la posicion nueva del arreglo, al user2 para que se conecten
+    friends_of_user1[current_size1].dest = user2;
+    // añade el peso que tiene la conexion al amigo
+    friends_of_user1[current_size1].weight = weight;
+    // la lista de adyacencia del grafo en la posicion user1, contendra el arreglo de estructura edge que contiene las conexiones
     graph->adyacent_friendship_list[user1] = friends_of_user1;
 
-    int *friends_of_user2 = graph->adyacent_friendship_list[user2];
-    friends_of_user2 = realloc(friends_of_user2, sizeof(int) * (graph->users_number + 1));
-    friends_of_user2[graph->users_number] = user1;
+    Edge *friends_of_user2 = graph->adyacent_friendship_list[user2];
+    friends_of_user2 = realloc(friends_of_user2, sizeof(Edge) * (current_size1 + 1));
+    friends_of_user2[current_size1].dest = user1;
+    friends_of_user2[current_size1].weight = weight;
     graph->adyacent_friendship_list[user2] = friends_of_user2;
 }
 
