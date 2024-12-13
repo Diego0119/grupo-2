1cb41a08c239448a2c99d72f69865969ee8d2bac
Iván Mansilla <ivan.mansillacor@gmail.com>
2024-12-13 19:42:54 -0300

    eliminando warnings

--- incs/database.h
+++ incs/database.h
@@ -67,7 +67,7 @@ int database_exists_and_not_empty(void);
 void load_all_users(PtrToHashTable table, Graph graph, GlobalInterests globalInterests);
 User load_user_from_file(const char *filename, PtrToHashTable table, Graph graph, GlobalInterests globalInterests);
 void load_database(PtrToHashTable table, Graph graph, GlobalInterests globalInterests);
-void load_connections(PtrToHashTable table, Graph graph, GlobalInterests globalInterests);
+void load_connections(PtrToHashTable table, GlobalInterests globalInterests);
 
 /* FUNCIONES DE SESION ACTUAL */
 void login(PtrToHashTable graph);
